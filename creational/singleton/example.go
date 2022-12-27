e85c154757d85563272b8017cd730170fb91bcb8
shengboyuan <15515272916@163.com>
2022-12-27 17:56:51 +0800

    fix panic: Singleton

--- creational/singleton/example.go
+++ creational/singleton/example.go
@@ -5,19 +5,25 @@ import (
 	"sync"
 )
 
-// Singleton
+// Singleton: Implementation 1
+// Use sync.Mutex as a simple Lock to control cocurrency.
 type Database struct {
 	connection string // I use 'string' to represent real dbConn here.
-	mutex      sync.Locker
+	mutex      sync.Mutex
 }
 
 func (d *Database) initialize() {
-	d.mutex.Lock()
-	defer d.mutex.Unlock()
-
 	if d.connection == "" {
-		d.connection = "ready"
-		fmt.Println("dbConn init finished")
+		d.mutex.Lock()
+		defer d.mutex.Unlock()
+		if d.connection == "" {
+			// Ensure that the instance hasn't yet been initialized by another thread
+			// while this one has been waiting for the lock's release.
+			d.connection = "ready"
+			fmt.Println("dbConn init finished")
+		} else {
+			fmt.Println("dbConn has been initialized as", d.connection)
+		}
 	} else {
 		fmt.Println("dbConn has been initialized as", d.connection)
 	}
@@ -42,6 +48,50 @@ func GetSomeData() {
 	for i := 0; i < 10; i++ {
 		go func() {
 			dbConn.initialize()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	dbConn.query("select * from users")
+}
+
+// Singleton: Implementation 2
+// Directly use sync.Once to ensure func 'getInstance()' execute only once.
+type Database2 struct {
+	connection string
+	once       sync.Once
+}
+
+func (d *Database2) getInstance() *Database2 {
+	d.once.Do(func() {
+		if d.connection == "" {
+			d.connection = "ready"
+			fmt.Println("dbConn initialization finished")
+		}
+	})
+	return d
+}
+
+func (d *Database2) query(sql string) {
+	if d.connection == "ready" {
+		fmt.Println("sql exec OK")
+	} else {
+		fmt.Println("dbConn hasn't been initialized!")
+	}
+}
+
+// Main Processing Flow
+func GetSomeData2() {
+	var wg sync.WaitGroup
+	dbConn := &Database2{}
+
+	dbConn.query("select * from users")
+
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			dbConn = dbConn.getInstance()
 			wg.Done()
 		}()
 	}
